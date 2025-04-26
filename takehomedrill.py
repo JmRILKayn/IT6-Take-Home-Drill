a5ae47a5ba8f9aa229f7c1017b1879e1461fa706
JM MAYORES <josemiguelmayores@gmail.com>
2025-04-26 17:34:34 +0800

    Basic socket connection to server

--- takehomedrill.py
+++ takehomedrill.py
@@ -1,1 +1,19 @@
- 
+import socket
+
+def send_post_request(pin):
+    host = "127.0.0.1"
+    port = 8888
+
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.settimeout(5)
+        client_socket.connect((host, port))
+        client_socket.close()
+
+    except socket.error as e:
+        print(f"Socket error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    send_post_request(0) 
