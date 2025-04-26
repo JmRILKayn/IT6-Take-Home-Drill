be5e9dd1d6613450468cd4b4419b8319ff838c09
JM MAYORES <josemiguelmayores@gmail.com>
2025-04-26 17:35:39 +0800

    Send HTTP POST request to /verify endpoint

--- takehomedrill.py
+++ takehomedrill.py
@@ -3,11 +3,17 @@ import socket
 def send_post_request(pin):
     host = "127.0.0.1"
     port = 8888
+    resource = "/verify"
+
+    form_data = f"magicNumber={pin:03d}"
+    request = f"""POST {resource} HTTP/1.1\r\nHost: {host}:{port}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {len(form_data)}\r\nConnection: close\r\n\r\n{form_data}"""
 
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.settimeout(5)
         client_socket.connect((host, port))
+        
+        client_socket.sendall(request.encode())
         client_socket.close()
 
     except socket.error as e:
@@ -16,4 +22,4 @@ def send_post_request(pin):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    send_post_request(0) 
+    send_post_request(0)
